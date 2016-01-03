9e9c5ac1be305ce47d281bb1fa7a0d606c23612b
Thanos K <pirouni@gmail.com>
2016-01-03 14:41:42 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+### Static Project Boilerplate with GULP
+
+This is a basic static website HTML5 boilerplate with Gulp intergration to automate some stuff like
+
+* SASS compile/minification
+* JS concatenation/minification
+
+This is my basic workflow for a project start.
+
+This repository creates a basic directory structure with a ```src``` and an ```assets``` folder for the concatenated/compiled/minified files. Your working folder will be the `src`.
+Has a prestructured sass folder with a _reset.scss partial to get you started and a simple js folder with a `console.log();`.
+All files in the js folder will be concatenated and minified.
+Has also the [https://github.com/afarkas/html5shiv](html5shiv)(HTML5 elements for older browsers) and [https://github.com/scottjehl/Respond](respond.js)(media queries polyfill).
+
+Your index.html is your main file that includes all the static assets.
+
+```
+  |-- index.html
+  |-- assets
+  |   |-- css
+  |   |   |-- style.css
+  |   |-- img
+  |   |-- js
+  |       |-- scripts.js
+  |       |-- vendor
+  |           |-- html5shiv.min.js
+  |           |-- respond.min.js
+  |-- src
+      |-- js
+      |   |-- script.js
+      |-- sass
+          |-- style.scss
+          |-- partials
+              |-- _reset.scss
+```
+
+### Installation
+
+```npm install```
+
+### Usage
+
+Just type ```gulp``` and Gulp will watch your files for changes.
---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+// Get all the required packages
+const   gulp = require('gulp'),
+        concat = require('gulp-concat'),
+        uglify = require('gulp-uglify'),
+        sass = require('gulp-sass'),
+        rename = require('gulp-rename'),
+        plumber = require('gulp-plumber'),
+        gutil = require('gulp-util'),
+        minifyCss = require('gulp-minify-css');
+
+// Set the path variables
+const   base_path = './',
+        src = base_path + 'src',
+        dist = base_path + 'assets',
+        paths = {  
+            js: src + '/js/*.js',
+            scss: [ src +'/sass/*.scss', src +'/sass/**/*.scss', src +'/sass/**/**/*.scss'],
+            html: src + '/**/*.html'
+        };
+
+// Combine & Compress JS into one script.js
+gulp.task('combine-js', () => {  
+  return gulp.src(paths.js)
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(dist + '/js'));
+});
+
+// Compile sass to css
+gulp.task('compile-sass', () => {  
+  return gulp.src(paths.scss)
+    .pipe(plumber((error) => {
+        gutil.log(gutil.colors.red(error.message));
+        gulp.task('compile-sass').emit('end');
+    }))
+    .pipe(sass())
+    .pipe(rename({dirname: dist + '/css'}))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./'));
+});
+
+// Watch files
+gulp.task('watch', () => {  
+  gulp.watch(paths.js, ['combine-js']);
+  gulp.watch(paths.scss, ['compile-sass']);
+});
+
+// Start Everything with the default task
+gulp.task('default', [ 'combine-js', 'compile-sass', 'watch' ]);
